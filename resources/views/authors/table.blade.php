f6e2ae9249e51e15dff93830263418016b26cf84
Mario Hunter <mario.hany.13@gmail.com>
2019-05-10 18:01:58 +0200

    scoped index

--- resources/views/authors/table.blade.php
+++ resources/views/authors/table.blade.php
@@ -13,6 +13,7 @@
                 {!! Form::open(['route' => ['authors.destroy', $author->id], 'method' => 'delete']) !!}
                 <div class='btn-group'>
                     <a href="{!! route('authors.show', [$author->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
+                    <a href="{!! route('books.index', ['author'=>$author->name]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"> See author's books</i></a>
                     <a href="{!! route('authors.edit', [$author->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
                     {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
                 </div>
